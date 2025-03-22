2fa8c7a41742cac51d0867b784422a4b5969261c
quentin0x00 <rouquette.quentin@outlook.fr>
2025-03-22 22:38:09 +0100

    v1

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CSV_FILE = 'data.csv'
 
 def load_data_from_csv():
-    """Charge les données depuis le fichier CSV."""
+
     chapters = {}
     chapter_content = []
     markers = []
@@ -28,22 +28,22 @@ def load_data_from_csv():
                 chapter_content.append({
                     'id': chapter_id,
                     'title': row['title'],
-                    'role': row.get('role', ''),  # Ajouter le rôle
-                    'date': row.get('date', ''),  # Ajouter la date
-                    'content': row.get('content', ''),  # Ajouter le contenu
-                    'skills': row.get('skills', ''),  # Ajouter les compétences
-                    'link': row.get('link', ''),  # Ajouter le lien
-                    'link_alias': row.get('link_alias', ''),  # Ajouter l'alias du lien
-                    'url_img': row.get('url_img', ''),  # Ajouter l'URL de l'image
-                    'border_color': row.get('border_color', '')  # Ajouter la couleur de bordure
+                    'role': row.get('role', ''),
+                    'date': row.get('date', ''),
+                    'content': row.get('content', ''),
+                    'skills': row.get('skills', ''),
+                    'link': row.get('link', ''),
+                    'link_alias': row.get('link_alias', ''),
+                    'url_img': row.get('url_img', ''),
+                    'border_color': row.get('border_color', '')
                 })
                 markers.append({
                     'id': chapter_id,
                     'lng': float(row['longitude']),
                     'lat': float(row['latitude']),
                     'title': row['title'],
-                    'url_img': row.get('url_img', ''),  # Ajouter l'URL de l'image
-                    'border_color': row.get('border_color', '')  # Ajouter la couleur de bordure
+                    'url_img': row.get('url_img', ''),
+                    'border_color': row.get('border_color', '')
                 })
     except FileNotFoundError:
         print(f"Le fichier {CSV_FILE} n'a pas été trouvé.")
@@ -54,10 +54,7 @@ def load_data_from_csv():
 
 @app.route('/')
 def index():
-    """Affiche la page principale avec les données du CSV."""
     chapters, chapter_content, markers = load_data_from_csv()
-
-    # Conversion en JSON des données des chapitres et des marqueurs
     chapters_json = json.dumps(chapters)
     markers_json = json.dumps(markers)
 
@@ -67,23 +64,22 @@ def index():
                            markers_json=markers_json)
 
 def create_csv_if_not_exists():
-    """Crée un fichier CSV exemple si le fichier n'existe pas déjà."""
     if not os.path.exists(CSV_FILE):
         try:
             with open(CSV_FILE, 'w', newline='', encoding='utf-8') as file:
                 writer = csv.writer(file)
                 writer.writerow(['id', 'title', 'role', 'date', 'content', 'skills', 'bearing', 'longitude', 'latitude', 'zoom', 'pitch', 'speed', 'link', 'link_alias', 'url_img', 'border_color'])
                 writer.writerows([
-                    ['0', 'Bonjour ! 👋', "Géomaticien basé sur Nantes.", "", "J'administre un SGBD spatial, automatise les traitements, et gère un portail web-cartographique.","PostgreSQL ; Python ; GeoServer ; Qgis ; Gdal/Ogr ; Vmap ; Maplibre ; Flask  ; Git ; Docker ; Linux", '0', '-1.553603493340308', '47.218599718064844', '16', '60', '1.3', '#', 'Contacter', '/static/img/moi.png', '#C84D82'],
-                    ['1', 'Abei Energy ⚡️', "Chargé d'études SIG (cdi - poste actuel)", "02|2023 - Présent", '> Automatisation du traitement de données  ; > Développement et maintenance du webSIG de l\'entreprise', "PostgreSQL ; Python ; GeoServer ; Qgis ; Gdal/Ogr ; Vmap ; Git ; Docker",'0', '-3.6914852859185343', '40.42880022606314', '16', '60', '1.3', '#', 'Abei Energy', '/static/img/abei.png', '#F4A261'],
-                    ['2', 'Thema Environnement 🌱', "Géomaticien (cdd - 6 mois)", "05|2022 - 11|2022", '> Traitement de données; > Analyses spatiales; > Atlas et cartographies', "PostgreSQL; Qgis ; Arcgis ; Gdal; Adobe Illustrator", '0', '-1.1857005797117022', '47.40885995562285', '16', '60', '1.3', '#', 'Thema Environnement', '/static/img/thema.png', '#37A77B'],
-                    ['3', 'GIP Littoral 🌊', "Chargé d'études SIG (cdd - 6 mois & stage - 6 mois)", "03|2021 - 04|2022", '> Traitement de données; > Analyses spatiales; > Atlas et cartographies', "PostgreSQL; Qgis ; Gdal; Adobe Illustrator", '0', '-0.5513001527871958', '44.82764741819648', '16', '60', '1.3', '#', 'Gip Littoral', '/static/img/gip.png', '#4575D4'],
-                    ['4', 'Université de Nantes IGARUN 📚', "Master 2 - SIG", "09|2016 - 09|2021", '> Traitement de données; > Analyses spatiales; > Atlas et cartographies', "PostgreSQL; Python ; Qgis ; Adobe Illustrator", '0', '-1.5492718817522353', '47.24614588519798', '16', '60', '1.3', '#', 'Université de Nantes', '/static/img/un.png', '#87a7fa']
+                    ['0', 'Bonjour ! 👋', "Géomaticien basé sur Nantes.", "", "J'administre un SGBD spatial, automatise les traitements, et gère un portail web-cartographique.","PostgreSQL ; Python ; GeoServer ; Qgis ; Gdal/Ogr ; Vmap ; Maplibre ; Flask  ; Git ; Docker ; Linux", '0', '-1.553603493340308', '47.218599718064844', '16', '60', '1.3', '#', 'Contacter 📧', '/static/img/moi.png', '#C84D82'],
+                    ['1', 'Abei Energy ⚡️', "Chargé d'études SIG (cdi - poste actuel)", "02|2023 - Présent", '> Automatisation du traitement de données  ; > Développement et maintenance du webSIG de l\'entreprise', "PostgreSQL ; Python ; GeoServer ; Qgis ; Gdal/Ogr ; Vmap ; Git ; Docker",'0', '-3.6914852859185343', '40.42880022606314', '16', '60', '1.3', '#', 'Abei Energy 🌐', '/static/img/abei.png', '#F4A261'],
+                    ['2', 'Thema Environnement 🌱', "Géomaticien (cdd - 6 mois)", "05|2022 - 11|2022", '> Traitement de données; > Analyses spatiales; > Atlas et cartographies', "PostgreSQL; Qgis ; Arcgis ; Gdal; Adobe Illustrator", '0', '-1.1857005797117022', '47.40885995562285', '16', '60', '1.3', '#', 'Thema Environnement 🌐', '/static/img/thema.png', '#37A77B'],
+                    ['3', 'GIP Littoral 🌊', "Chargé d'études SIG (cdd - 6 mois & stage - 6 mois)", "03|2021 - 04|2022", '> Traitement de données; > Analyses spatiales; > Atlas et cartographies', "PostgreSQL; Qgis ; Gdal; Adobe Illustrator", '0', '-0.5513001527871958', '44.82764741819648', '16', '60', '1.3', '#', 'Gip Littoral 🌐', '/static/img/gip.png', '#4575D4'],
+                    ['4', 'Université de Nantes IGARUN 📚', "Master 2 - SIG", "09|2016 - 09|2021", '> Traitement de données; > Analyses spatiales; > Atlas et cartographies', "PostgreSQL; Python ; Qgis ; Adobe Illustrator", '0', '-1.5492718817522353', '47.24614588519798', '16', '60', '1.3', '#', 'Université de Nantes 🌐', '/static/img/un.png', '#87a7fa']
                 ])
             print(f"Fichier {CSV_FILE} créé avec des données d'exemple.")
         except Exception as e:
             print(f"Erreur lors de la création du fichier CSV : {e}")
 
 if __name__ == '__main__':
-    create_csv_if_not_exists()  # Créer le fichier CSV si nécessaire
+    create_csv_if_not_exists()
     app.run(debug=True)
--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Remonter au premier élément (section) au chargement de la page
-    const firstChapterId = Object.keys(chapters)[0]; // Récupère l'ID du premier chapitre
+
+    /* Réinitialiser le panneau au chargement de la page */
+    const firstChapterId = Object.keys(chapters)[0];
     const firstSection = document.getElementById(firstChapterId);
     if (firstSection) {
-        firstSection.scrollIntoView({ behavior: 'smooth' }); // Défilement vers le premier élément
+        firstSection.scrollIntoView({ behavior: 'smooth' });
     }
 
-    // Initialisation de la carte
+
+    /* Carte */
     const map = new maplibregl.Map({
         container: 'map',
         style: {
@@ -34,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     map.on('style.load', () => {
         map.setProjection({
-            type: 'globe', // Set projection to globe
+            type: 'globe',
         });
     });
 
@@ -48,42 +50,6 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    function createCustomMarker(marker) {
-        const el = document.createElement('div');
-        el.style.width = '50px'; // Taille du marqueur
-        el.style.height = '50px';
-        el.style.borderRadius = '50%'; // Forme ronde
-        el.style.border = `6px solid ${marker.border_color}`; // Bordure avec la couleur spécifiée
-        el.style.backgroundImage = `url('${marker.url_img}')`; // Image de fond
-        el.style.backgroundSize = '100%'; // Dézoomer l'image (réduire sa taille à 80%)
-        el.style.backgroundPosition = 'center'; // Centrer l'image
-        el.style.backgroundColor = '#fff'; // Fond blanc
-        el.style.backgroundRepeat = 'no-repeat'; 
-        el.style.cursor = 'pointer'; // Curseur en forme de main au survol
-        return el;
-    }
-    
-
-    // Ajout des marqueurs à la carte
-    markers.forEach(marker => {
-        const markerElement = new maplibregl.Marker({
-            element: createCustomMarker(marker) // Utiliser l'élément personnalisé
-        })
-        .setLngLat([marker.lng, marker.lat])
-        .addTo(map);
-
-        // Ajouter un événement "click" au marqueur
-        markerElement.getElement().addEventListener('click', () => {
-            console.log('Marqueur cliqué :', marker.id); // Vérifiez que l'événement est déclenché
-            const sectionId = marker.id; // Assurez-vous que `marker.id` correspond à l'ID de la section
-            const section = document.getElementById(sectionId);
-            if (section) {
-                section.scrollIntoView({ behavior: 'smooth' });
-            }
-        });
-    });
-
-    // Ajouter les contrôles de navigation et d'échelle
     map.addControl(new maplibregl.NavigationControl(), 'top-right');
     map.addControl(new maplibregl.ScaleControl({ maxWidth: 80, unit: "metric" }), 'bottom-left');
     map.addControl(new maplibregl.AttributionControl({
@@ -93,17 +59,12 @@ document.addEventListener('DOMContentLoaded', function () {
         ]
     }), 'bottom-right');
 
-    // Fonction pour ajouter les couches supplémentaires (bâtiments 3D, etc.)
     function addExtraLayers() {
         add3DBuildings();
-        // addBoundaryLines(); // Décommentez si vous avez une fonction pour ajouter des lignes de limites
     }
 
-    // Fonction pour ajouter les bâtiments 3D
     function add3DBuildings() {
         if (map.getLayer('3d-buildings')) return;
-
-        // Source OpenMapTiles
         if (!map.getSource('openmaptiles')) {
             map.addSource('openmaptiles', {
                 type: 'vector',
@@ -113,8 +74,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 attribution: "© <a href='https://openfreemap.org/' target='_blank'>OpenFreeMap</a>"
             });
         }
-
-        // Couche des bâtiments 3D
         map.addLayer({
             id: '3d-buildings',
             type: 'fill-extrusion',
@@ -122,7 +81,7 @@ document.addEventListener('DOMContentLoaded', function () {
             'source-layer': 'building',
             minzoom: 14,
             paint: {
-                'fill-extrusion-color': '#DCD9D6', // Couleur des bâtiments
+                'fill-extrusion-color': '#DCD9D6',
                 'fill-extrusion-height': [
                     "case",
                     ["has", "render_height"], ["get", "render_height"],
@@ -132,13 +91,48 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     }
-
-    // Appliquer le fond de carte et ajouter les couches supplémentaires
     map.once('idle', function () {
         addExtraLayers();
     });
 
-    // Gestion des chapitres (sections)
+
+
+    /* Markers */
+    function createCustomMarker(marker) {
+        const el = document.createElement('div');
+        el.style.width = '50px';
+        el.style.height = '50px';
+        el.style.borderRadius = '50%';
+        el.style.border = `6px solid ${marker.border_color}`;
+        el.style.backgroundImage = `url('${marker.url_img}')`;
+        el.style.backgroundSize = '100%';
+        el.style.backgroundPosition = 'center';
+        el.style.backgroundColor = '#fff';
+        el.style.backgroundRepeat = 'no-repeat'; 
+        el.style.cursor = 'pointer';
+        return el;
+    }
+    
+    markers.forEach(marker => {
+        const markerElement = new maplibregl.Marker({
+            element: createCustomMarker(marker)
+        })
+        .setLngLat([marker.lng, marker.lat])
+        .addTo(map);
+
+        // Interaction marker - panneau
+        markerElement.getElement().addEventListener('click', () => {
+            console.log('Marqueur cliqué :', marker.id);
+            const sectionId = marker.id;
+            const section = document.getElementById(sectionId);
+            if (section) {
+                section.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+
+
+    /* Panneau */
     let activeChapterName = Object.keys(chapters)[0];
     setActiveChapter(activeChapterName);
 
@@ -150,7 +144,7 @@ document.addEventListener('DOMContentLoaded', function () {
         activeChapterName = chapterName;
     }
 
-    // Vérifie si un élément est visible à l'écran
+    // Vérifie si un élément est visible au sein de #features
     function isElementOnScreen(id) {
         const element = document.getElementById(id);
         if (!element) return false;
@@ -164,7 +158,7 @@ document.addEventListener('DOMContentLoaded', function () {
             elementOffsetTop + elementHeight - featuresScrollTop > threshold;
     }
 
-    // Gestion du défilement pour changer de chapitre
+    // Gestion du défilement pour changer d'encart
     document.getElementById('features').onscroll = function () {
         const chapterNames = Object.keys(chapters);
         for (const chapterName of chapterNames) {
@@ -176,7 +170,7 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 });
 
-// Fonctions pour la navigation entre les sections
+// Boutons de navigation du panneau
 function scrollToPreviousSection(currentSectionId) {
     const currentSection = document.getElementById(currentSectionId);
     if (!currentSection) return;
